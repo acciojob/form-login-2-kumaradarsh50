e741fd7faacf18e20dc5c84930e4e1c75e90503a
Adarsh kumar <62237724+kumaradarsh50@users.noreply.github.com>
2023-03-12 22:30:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const submitBtn = document.getElementById('btn');
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const firstName = document.getElementById('firstname').value;
+  const lastName = document.getElementById('lastname').value;
+  const phoneNumber = document.getElementById('phonenumber').value;
+  const email = document.getElementById('email').value;
+
+  const mesg = `First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${email}`;
+
+  if (!firstName || !lastName || !phoneNumber || !email) {
+    return alert(`Please fill out all fields.`);
+  } else {
+    return alert(mesg);
+  }
+});
